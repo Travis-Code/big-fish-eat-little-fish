cc9382020521a0dd3a6d55486050a9c5e64ea4ec
Trav <jorkilla@gmail.com>
2016-07-12 18:42:17 -1000

    added more features
    new features

--- game.js
+++ game.js
@@ -19,6 +19,7 @@ boot.prototype = {
 	preload: function(){
 		this.game.load.image("loading","assets/loading.png");
 		this.game.load.image("fish","assets/fish.png");
+		this.game.load.image("blueFish","assets/blueFish.png");
 		this.game.load.image("ground","assets/ground.png");
 	},
 
@@ -41,7 +42,7 @@ preload.prototype = {
 
 	create: function(){
 		game.state.start("TitleScreen");
-		}
+	}
 }
 
 var titleScreen = function(game){};
@@ -53,31 +54,69 @@ titleScreen.prototype = {
 }
 
 var playGame = function(game){};
-titleScreen.prototype = {
+playGame.prototype = {
 	create:function(){
-
+		
+		this.eaten = 0;
+		this.scaleFishy = 110;
 		this.randomFish = Math.random()*5;
+		this.randomBlueFishSize = Math.random()*2;
 
-	 	// The baddies!
+//The score------------------------------------------------
+		var style = { font: "65px Helvetica", fill: "#ff0044", align: "center" };
+	    var text = game.add.text(game.world.width/3, game.world.height/4, "eaten:"+ this.eaten , style);
+	    text.anchor.set(0.5);
+
+//The baddies------------------------------------------------
 	    this.badFishes = game.add.group();
 	    this.badFishes.enableBody = true;
 	    this.badFishes.physicsBodyType = Phaser.Physics.ARCADE;
-	    this.createBadFish();
+	    // this.createBadFish();
 
-	    //this.badFishes = this.game.time.events.loop(Phaser.Timer.SECOND * 5, this.createBarrel, this)
+	    // this.badFishes = this.game.time.events.loop(Phaser.Timer.SECOND * 5, this.createBarrel, this)
 
+//blue fish-------------------------------------------------
 
 		game.physics.startSystem(Phaser.Physics.ARCADE);
-		var ground = this.add.sprite(game.width/2, game.height-100,"ground");
-		this.fish = this.add.sprite(game.width/2, game.height/2,"fish");
+
+		this.blueFish = this.add.sprite(game.width/2, game.height/2,"blueFish");
+  		game.physics.arcade.enable(this.blueFish);
+  		this.blueFish.scale.setTo(this.randomBlueFishSize, this.randomBlueFishSize);
+ 		
+ 		game.time.events.add(Phaser.Timer.SECOND * 4, this.addBlueFish, this);
+
+		
+//ground-----------------------------------------------------
+		this.ground = this.add.sprite(game.width/2, game.height-100,"ground");
+  		game.physics.arcade.enable(this.ground);
+  		this.ground.body.allowGravity = false;
+    	this.ground.body.immovable = true;
+//PLAYER FISH------------------------------------------------
+		this.fish = this.add.sprite(game.width/2, game.height/3,"fish");
 	    this.fish.anchor.set(0.5);
 	    game.physics.arcade.enable(this.fish);
-		console.log("hello PlayGame");
+	   	this.fish.body.collideWorldBounds=true;
+
 		console.log(this.randomFish);
 
 	},
 
 	update:function() {
+
+		this.blueFish.x += .08;
+		if (this.blueFish.x >= game.width + this.blueFish.width){
+			this.blueFish.destroy();
+		}
+
+		game.physics.arcade.collide(this.fish, this.ground);
+    	game.physics.arcade.overlap(this.fish, this.blueFish, this.playerAndBlueCollide, null, this);
+
+
+
+
+
+
+//CONTROLS-------------------------------------------------------------------------------------
 	    //  If the sprite is > 8px away from the pointer then let's move to it
 	    if (game.physics.arcade.distanceToPointer(this.fish, game.input.activePointer) > 8){
 	        //  Make the object seek to the active pointer (mouse or touch).
@@ -85,23 +124,44 @@ titleScreen.prototype = {
 	    }else{
 	        //  Otherwise turn off velocity because we're close enough to the pointer
 	        this.fish.body.velocity.set(0);
-
 	    }
 	},
 
+	playerAndBlueCollide: function() {
+	    //console.log(this.randomFish);
+	    if(this.fish.width <= this.blueFish.width){
+	    	this.fish.scale.setTo(this.fish.scale.x+ 1, this.fish.scale.y+ 1);
+	    	this.blueFish.destroy();
+
+	    } else{
+	    	console.log(this.eaten);
+			this.fish.scale.setTo(this.fish.scale.x+ 1, this.fish.scale.y+ 1);
+	    	this.blueFish.destroy();
+	    }
+	 },
+
 	descend:function() {
-	    this.badFishes.y += 110;
+	    this.badFishes.body.y += 110;
 	},
+
+	addBlueFish:function(){
+		// var randomValue = game.rnd.integerInRange(0, 25);
+		this.newBlueFish = this.add.sprite(game.width/2, game.height/2,"blueFish");
+  		game.physics.arcade.enable(this.newBlueFish);
+  		this.newBlueFish.scale.setTo(this.randomBlueFishSize, this.randomBlueFishSize);
+
+	},
+
 
 	createBadFish:function() {
 	    for (var y = 0; y < 4; y++){
 	        for (var x = 0; x < 10; x++)
 	        {
-	            this.alien = this.badFishes.create(x * 68, y * 68, 'fish');
-	            this.alien.anchor.setTo(0.5, 0.5);
-	            //this.alien.animations.add('fly', [ 0, 1, 2, 3 ], 20, true);
-	            //this.alien.play('fly');
-	            this.alien.body.moves = false;
+	            this.badFishCreate = this.badFishes.create(x * 75, y * 75, 'fish');
+	            this.badFishCreate.anchor.setTo(0.5, 0.5);
+	            //this.badFishCreate.animations.add('fly', [ 0, 1, 2, 3 ], 20, true);
+	            //this.badFishCreate.play('fly');
+	            this.badFishCreate.body.moves = false;
 	        }
 	    }
 	    this.badFishes.x = 100;
